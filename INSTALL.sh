74fb2da47eb6c2872a63f6087e3e13872821fdd2
Andreas <adde88@gmail.com>
2017-01-13 14:41:34 +0100

    Improved install-script

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,11 +5,20 @@
 #
 # Starting MANA-Toolkit Install.
 #
-# Download installation-files to temporary directory.
+# Variables and colors.
+RED='\033[0;31m'
+NC='\033[0m'
+mana_version="2017-01-13"
+MANA="https://github.com/adde88/hostapd-mana-openwrt/raw/master/bin/ar71xx/packages/base/hostapd-mana_"$mana_version"_ar71xx.ipk"
+ASLEAP="https://github.com/adde88/hostapd-mana-openwrt/raw/master/bin/ar71xx/packages/base/asleap_2.2-1_ar71xx.ipk"
+#
+echo -e "${RED}Installing: ${NC}MANA-Toolkit."
+echo -e "Go grab a cup of coffee, this can take a little while...\n"
+# Download installation-files to temporary directory, and then update OPKG repositories.
 cd /tmp
 opkg update
-wget https://github.com/adde88/hostapd-mana-openwrt/raw/master/bin/ar71xx/packages/base/asleap_2.2-1_ar71xx.ipk
-wget https://github.com/adde88/hostapd-mana-openwrt/raw/master/bin/ar71xx/packages/base/hostapd-mana_2017-01-13_ar71xx.ipk
+wget "$ASLEAP"
+wget "$MANA"
 #
 # Creating sym-link between python-directories located on the sd-card and internally.
 # The main-directory will be located on the sd-card (/sd)
@@ -19,9 +28,13 @@ if [ -e /sd ]; then
 	rm -r /usr/lib/python2.7
 	mkdir -p /sd/usr/lib/python2.7
 	ln -s /sd/usr/lib/python2.7 /usr/lib/python2.7
-	opkg --dest sd --force-overwrite install asleap_2.2-1_ar71xx.ipk hostapd-mana_2017-01-13_ar71xx.ipk
+	opkg --dest sd --force-overwrite install asleap_2.2-1_ar71xx.ipk hostapd-mana_"$mana_version"_ar71xx.ipk
 else
 	# Tetra installation / general install.
-	opkg --force-overwrite install asleap_2.2-1_ar71xx.ipk hostapd-mana_2017-01-13_ar71xx.ipk
+	opkg --force-overwrite install asleap_2.2-1_ar71xx.ipk hostapd-mana_"$mana_version"_ar71xx.ipk
 fi
+# Cleanup
+rm hostapd-mana_"$mana_version"_ar71xx.* asleap_2.2-1_ar71xx.*
+echo -e "${RED}Installation completed!"
+echo -e "${NC}Launch MANA by typing: '${RED}launch-mana' ${NC}in the terminal."
 exit 0
