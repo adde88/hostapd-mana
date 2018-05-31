0a52f573ce8b9318116846c2dd07e60df8e7a351
Andreas <adde88@gmail.com>
2018-05-31 11:05:36 +0200

    Let's avoid patching the source code during compilation

--- src/ap/ieee802_1x.c
+++ src/ap/ieee802_1x.c
@@ -2437,9 +2437,9 @@ void ieee802_1x_notify_pre_auth(struct eapol_state_machine *sm, int pre_auth)
 }
 
 
-static const char * bool_txt(Boolean val)
+static const char * bool_txt(Boolean bool_val)
 {
-	return val ? "TRUE" : "FALSE";
+        return bool_val ? "TRUE" : "FALSE";
 }
 
 
