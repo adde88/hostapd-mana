d6c59bf636ae0dd09a3a96cab3be83e92047b01d
Andreas Nilsen <adde88@gmail.com>
2020-04-11 21:58:03 +0200

    update
    Signed-off-by: Andreas Nilsen <adde88@gmail.com>

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #
-# Installation script for MANA-Toolkit, for the Pineapple NANO and TETRA.
+# Installation script for hostapd-mana, for the Pineapple NANO and TETRA.
 # Written by: Andreas Nilsen - adde88@gmail.com - https://www.github.com/adde88
-#
-# Starting MANA-Toolkit Install.
 #
 # Variables and colors.
 RED='\033[0;31m'
@@ -14,7 +12,7 @@ wget https://github.com/adde88/hostapd-mana-openwrt/tree/master/bin/ar71xx/packa
 MANA=`grep -F "hostapd-mana_" /tmp/ManaToolkit/base | awk {'print $5'} | awk -F'"' {'print $2'} | grep ar71xx`
 ASLEAP=`grep -F "asleap_" /tmp/ManaToolkit/base | awk {'print $5'} | awk -F'"' {'print $2'} | grep ar71xx`
 #
-echo -e "${RED}Installing: ${NC}MANA-Toolkit."
+echo -e "${RED}Installing: ${NC}hostapd-mana."
 echo -e "Go grab a cup of coffee, this will take a while...\n"
 # Download installation-files to temporary directory, and then update OPKG repositories.
 cd /tmp
@@ -28,7 +26,7 @@ wget "https://github.com/adde88/hostapd-mana-openwrt/raw/master/bin/ar71xx/packa
 if [ -e /sd ]; then
 	# sym-link & nano install
 	opkg --dest sd --force-overwrite install "$ASLEAP" "$MANA" sslsplit
-	ln -s /sd/etc/mana-toolkit /etc/mana-toolkit
+	ln -s /sd/etc/hostapd-mana /etc/hostapd-mana
 else
 	# Tetra installation / general install.
 	opkg --force-overwrite install "$ASLEAP" "$MANA" sslsplit
@@ -39,7 +37,7 @@ rm -f "$ASLEAP" "$MANA"
 /etc/init.d/stunnel disable
 echo -e "${RED}Installation completed!"
 echo -e "${NC}Type: ${RED}'install-mana-depends' ${NC}to install python-related dependencies."
-echo -e "${NC}Launch MANA by typing: '${RED}launch-mana' ${NC}in the terminal."
+echo -e "${NC}Launch hostapd-mana by typing: '${RED}launch-mana' ${NC}in the terminal."
 # Let's set the default interface
 uci set ManaToolkit.run.interface="wlan1"
 uci commit ManaToolkit.run.interface
