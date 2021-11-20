b2b228190465dce6e856615367240950e862d6b3
Andreas Nilsen <adde88@gmail.com>
2021-11-20 16:16:23 +0100

    Update INSTALL.sh
    Signed-off-by: Andreas Nilsen <adde88@gmail.com>

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,29 +6,41 @@
 # Variables and colors.
 RED='\033[0;31m'
 NC='\033[0m'
+MACHINE=`cat /proc/cpuinfo | grep machine | awk -F":" {'print $2'}`
+case "$MACHINE" in
+  *Mark7*)
+    CPU='mipsel_24kc'
+  ;;
+  *TETRA*)
+    CPU='mips_24kc'
+  ;;
+  *NANO*)
+    CPU='mips_24kc'
+  ;;
+esac
 #
 mkdir -p /tmp/ManaToolkit
 wget https://github.com/adde88/hostapd-mana-openwrt/tree/openwrt-19.07/bin/packages/mipsel_24kc/custom -P /tmp/ManaToolkit 2&>1 >/dev/null
-MANA=`grep -F "hostapd-mana_" /tmp/ManaToolkit/custom | awk {'print $8'} | awk -F'"' {'print $2'} | grep mipsel_24kc`
-ASLEAP=`grep -F "asleap_" /tmp/ManaToolkit/custom | awk {'print $8'} | awk -F'"' {'print $2'} | grep mipsel_24kc`
+MANA=`grep -F "hostapd-mana_" /tmp/ManaToolkit/custom | awk {'print $8'} | awk -F'"' {'print $2'} | grep $CPU`
+ASLEAP=`grep -F "asleap_" /tmp/ManaToolkit/custom | awk {'print $8'} | awk -F'"' {'print $2'} | grep $CPU`
 #
 echo -e "${RED}Installing: ${NC}hostapd-mana."
 echo -e "Go grab a cup of coffee, this will take a while...\n"
 # Download installation-files to temporary directory, and then update OPKG repositories.
 cd /tmp
 opkg update
-wget "https://github.com/adde88/hostapd-mana-openwrt/raw/openwrt-19.07/bin/packages/mipsel_24kc/custom/"$ASLEAP"" 2&>1 >/dev/null
-wget "https://github.com/adde88/hostapd-mana-openwrt/raw/openwrt-19.07/bin/packages/mipsel_24kc/custom/"$MANA"" 2&>1 >/dev/null
+wget "https://github.com/adde88/hostapd-mana-openwrt/raw/openwrt-19.07/bin/packages/"$CPU"/custom/"$ASLEAP"" 2&>1 >/dev/null
+wget "https://github.com/adde88/hostapd-mana-openwrt/raw/openwrt-19.07/bin/packages/"$CPU"/custom/"$MANA"" 2&>1 >/dev/null
 #
 # Creating sym-link between python-directories located on the sd-card and internally.
 # The main-directory will be located on the sd-card (/sd)
 # This will only happen on the Pineapple NANO.
 if [ -e /sd ]; then
-	# sym-link & nano install
+	# sym-link / NANO install
 	opkg --dest sd --force-overwrite install "$ASLEAP" "$MANA" sslsplit
 	ln -s /sd/etc/hostapd-mana /etc/hostapd-mana
 else
-	# Tetra installation / general install.
+	# General install
 	opkg --force-overwrite install "$ASLEAP" "$MANA" sslsplit
 fi
 # Cleanup
@@ -36,9 +48,8 @@ rm -f "$ASLEAP" "$MANA"
 # Disable stunnel init-script at boot.
 /etc/init.d/stunnel disable
 echo -e "${RED}Installation completed!"
-echo -e "${NC}Type: ${RED}'install-mana-depends' ${NC}to install python-related dependencies."
-echo -e "${NC}Launch hostapd-mana by typing: '${RED}launch-mana' ${NC}in the terminal.\n"
-echo -e "${RED}WARNING: ${NC}This script has not been updated for the Pineapple MK7 yet! And will most likely fail on this device!"
+echo -e "${NC}Launch hostapd-mana by typing: '${RED}hostapd-mana' ${NC}in the terminal.\n"
+echo -r "${NC}Or, you can use: '${RED}berate_ap'${NC}.\n"
 # Let's set the default interface
 uci set ManaToolkit.run.interface="wlan0"
 uci commit ManaToolkit.run.interface
